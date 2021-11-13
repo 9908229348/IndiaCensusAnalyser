458dc6c3a69522d83a8c563993921b0d50e1de57
9908229348 <kalavaisravanthi98@gmail.com>
2021-11-13 09:02:25 +0530

    Refactored: Test method to that returns exception when wrong file path

--- test/java/censusanalyser/CensusAnalyserTest.java
+++ test/java/censusanalyser/CensusAnalyserTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILEPATH = "./src/test/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
 
     @Test
     public void givenIndiaCensusCsvFile_ReturnsCorrectRecords() {
@@ -13,6 +14,17 @@ public class CensusAnalyserTest {
             int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILEPATH);
             Assert.assertEquals(29, numOfRecords);
         }catch (CensusAnalyserException e){ }
+    }
+
+    @Test
+    public void givenWrongIndiaCensusCsvFile_ReturnsException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(WRONG_FILE_PATH);
+            Assert.assertEquals(29, numOfRecords);
+        }catch (CensusAnalyserException e){
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
     }
 }
 
